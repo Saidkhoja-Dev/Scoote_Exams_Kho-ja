55dee67acaf30df534be71300a6d9ffda94d06a0
Saidaminxo'ja <saidaminxojam@gmail.com>
2022-06-22 17:49:00 +0500

    Frist commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const elHamburgerBtn = document.querySelector(".header__menu")
+
+const elHeader = document.querySelector(".header")
+
+elHamburgerBtn.addEventListener("click", () => {
+  elHeader.classList.toggle(".header--open")
+})
